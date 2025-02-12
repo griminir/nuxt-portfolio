72bb40edd1f7ab3512bdb19e52d42ffed760785a
griminir <viktor.elliot.king@gmail.com>
2025-02-12 12:18:56 +0100

    style scoped and creating tailwind classes

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,12 +1,18 @@
 <template>
   <nav class="font-mono">
     <ul class="flex space-x-4">
-      <li>
+      <li class="link">
         <NuxtLink to="/">Main</NuxtLink>
       </li>
-      <li>
+      <li class="link">
         <NuxtLink to="/about">About</NuxtLink>
       </li>
     </ul>
   </nav>
 </template>
+
+<style scoped>
+  .link {
+    @apply hover:text-orange-700 p-1 hover:bg-gray-200;
+  }
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,9 @@
   <div class="container mx-auto max-w-2xl">
     <header class="flex justify-between items-center mt-5">
       <div>
-        <NuxtLink to="/" class="text-2xl font-bold p-2 hover:bg-gray-200"
+        <NuxtLink
+          to="/"
+          class="text-2xl font-bold p-2 hover:bg-gray-200 hover:text-orange-700"
           >Viktor E. Degray</NuxtLink
         >
       </div>
