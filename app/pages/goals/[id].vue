80519f514650714ed7f65c5cf315f52435f8d4ad
griminir <viktor.elliot.king@gmail.com>
2025-02-12 11:33:52 +0100

    auto import and params for routing

---
+++ app/pages/goals/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>we are inside a goal</h1>
+  </div>
+  <div>
+    <h2>Goal ID: {{ route.params.id }}</h2>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+</script>
---
+++ app/pages/goals/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>hello from Goals</h1>
+  </div>
+</template>
