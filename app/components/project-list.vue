7afdad5e40166c25e3fba9d86a7ba410940265e4
griminir <viktor.elliot.king@gmail.com>
2025-02-13 12:47:58 +0100

    color mode button made

--- app/components/project-list.vue
+++ app/components/project-list.vue
@@ -1,10 +1,14 @@
 <template>
+  <p>Take a look at some of my GitHub projects!</p>
+  <br />
   <section v-if="pending">Loading...</section>
-  <section v-else-if="error"> error getting stuff</section>
+  <section v-else-if="error">error getting stuff</section>
   <section v-else>
-    <ul>
-      <li v-for="repo in data" :key="repo.id">
-        <a :href="repo.html_url">{{ repo.name }}</a>
+    <ul class="grid grid-cols-1 gap-4">
+      <li v-for="repo in dataToDisplay" :key="repo.id" class="link">
+        <a :href="repo.html_url" target="_blank">
+          <div class="font-semibold">{{ repo.name }}</div>
+        </a>
       </li>
     </ul>
   </section>
@@ -14,4 +18,14 @@
 const { error, data, pending } = await useFetch(
   'https://api.github.com/users/griminir/repos'
 );
+
+const dataToDisplay = computed(() =>
+  data.value.filter((repo) => repo.name !== 'griminir')
+);
 </script>
+
+<style scoped>
+.link {
+  @apply border border-gray-200 rounded-sm hover:bg-gray-100 font-mono items-center p-2 hover:text-orange-700;
+}
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="container mx-auto max-w-2xl">
     <header class="flex justify-between items-center mt-5">
-      <div>
-        <NuxtLink
-          to="/"
-          class="text-2xl font-bold p-2 hover:bg-gray-200 hover:text-orange-700"
-          >Viktor E. Degray</NuxtLink
-        >
+      <div class="flex items-center space-x-12">
+        <div>
+          <NuxtLink
+            to="/"
+            class="text-2xl font-bold p-2 hover:bg-gray-200 hover:text-orange-700"
+            >Viktor E. Degray</NuxtLink
+          >
+        </div>
+        <NavBar />
       </div>
-      <NavBar />
+      <ClientOnly>
+        <ColorModeSelector />
+      </ClientOnly>
     </header>
     <main class="p-2 mt-10">
       <slot />
@@ -36,5 +41,9 @@ useHead({
 <style>
 body {
   font-family: 'Roboto';
+}
+
+body {
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300;
 }
 </style>
